cd92700e7eac919c80a4fc51814d83c9520c6c5a
Carlos Bentin <cbentin12@gmail.com>
2022-03-07 08:39:51 -0500

    implementing first console methods quit, EOF, empty line, help

--- console.py
+++ console.py
@@ -5,8 +5,14 @@
 """
 
 import cmd
-import sys
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -23,6 +29,32 @@ class HBNBCommand(cmd.Cmd):
         return (arg.split())
 
     " ===================== Model Methods ============================ "
+
+    def do_quit(self, args):
+        """
+            Quit command to exit the program\n
+        """
+        return True
+
+    def do_EOF(self, args):
+        """
+            EOF command to exit the program\n
+        """
+        print("")
+        return True
+
+    def emptyline(self):
+        """
+            Does not perform any action
+        """
+        pass
+
+    def help_help(self):
+        """
+            Prints help command description
+        """
+        print("Provides description of a given command")
+
     def do_print(self, arg):
         _list = []
         _list = arg.split()
